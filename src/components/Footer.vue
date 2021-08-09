6d19bbab27950417448db9e37060c84844d62c8b
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2021-08-09 23:05:21 +0300

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer fixed-bottom bg-light">
+    <div class="container text-center">
+      <span class="text-muted">
+        Music:
+        <a href="https://www.bensound.com/royalty-free-music" target="_blank">
+          bensound.com
+        </a>
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
